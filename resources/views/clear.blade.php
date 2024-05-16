1124adf5ec90ff87826be7e04719d2b81ba9d6e0
auliaririsamandaa <191220056@unmuhpnk.ac.id>
2024-05-16 14:56:55 +0700

    Add files via upload

---
+++ resources/views/clear.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Selesai</title>
+</head>
+<style>
+    body, html {
+            height: 100%;
+            margin: 0;
+        }
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            text-align: center;
+            background-color: #f8f9fa; /* Optional: background color for better visibility */
+        }
+        h1, p {
+            margin: 0;
+        }
+</style>
+<body>
+    
+
+
+    <div class="container">
+        <div>
+            <h1>TERIMA KASIH TELAH MEMESAN BARANG DI TOKO KAMI</h1>
+            <p>Semoga berkah dan menjadi amal jariyah</p>
+        </div>
+    </div>
+
+<div class="d-flex justify-content-center mt-3">
+    <a href="home2" class="btn btn-primary btn-lg">Kembali</a>
+</div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
